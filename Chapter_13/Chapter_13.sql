525fee9ac29e5ea967381ead0056906750e29981
Mai Nguyen <imnguyenmai@gmail.com>
2020-11-27 19:06:56 +0700

    change how to parse datetime string with timezone set separately for data entry session

--- Chapter_13/Chapter_13.sql
+++ Chapter_13/Chapter_13.sql
@@ -70,7 +70,7 @@ CREATE TABLE crime_reports (
 );
 
 COPY crime_reports (original_text)
-FROM 'C:\YourDirectory\crime_reports.csv'
+FROM 'C:\Users\Public\crime_reports.csv'
 WITH (FORMAT CSV, HEADER OFF, QUOTE '"');
 
 SELECT original_text FROM crime_reports;
@@ -102,8 +102,8 @@ FROM crime_reports;
 SELECT
     regexp_match(original_text, '(?:C0|SO)[0-9]+') AS case_number,
     regexp_match(original_text, '\d{1,2}\/\d{1,2}\/\d{2}') AS date_1,
-    regexp_match(original_text, '\n(?:\w+ \w+|\w+)\n(.*):') AS crime_type,
-    regexp_match(original_text, '(?:Sq.|Plz.|Dr.|Ter.|Rd.)\n(\w+ \w+|\w+)\n')
+    regexp_match(original_text, '\r\n(?:\w+ \w+|\w+)\n(.*):') AS crime_type,
+    regexp_match(original_text, '(?:Sq.|Plz.|Dr.|Ter.|Rd.)\r\n(\w+ \w+|\w+)\r\n')
         AS city
 FROM crime_reports;
 
@@ -115,14 +115,14 @@ SELECT crime_id,
             THEN regexp_match(original_text, '-(\d{1,2}\/\d{1,2}\/\d{1,2})')
             ELSE NULL
             END AS date_2,
-       regexp_match(original_text, '\/\d{2}\n(\d{4})') AS hour_1,
+       regexp_match(original_text, '\/\d{2}\r\n(\d{4})') AS hour_1,
        CASE WHEN EXISTS (SELECT regexp_matches(original_text, '\/\d{2}\n\d{4}-(\d{4})'))
-            THEN regexp_match(original_text, '\/\d{2}\n\d{4}-(\d{4})')
+            THEN regexp_match(original_text, '\/\d{2}\r\n\d{4}-(\d{4})')
             ELSE NULL
             END AS hour_2,
-       regexp_match(original_text, 'hrs.\n(\d+ .+(?:Sq.|Plz.|Dr.|Ter.|Rd.))') AS street,
-       regexp_match(original_text, '(?:Sq.|Plz.|Dr.|Ter.|Rd.)\n(\w+ \w+|\w+)\n') AS city,
-       regexp_match(original_text, '\n(?:\w+ \w+|\w+)\n(.*):') AS crime_type,
+       regexp_match(original_text, 'hrs.\r\n(\d+ .+(?:Sq.|Plz.|Dr.|Ter.|Rd.))') AS street,
+       regexp_match(original_text, '(?:Sq.|Plz.|Dr.|Ter.|Rd.)\r\n(\w+ \w+|\w+)\n') AS city,
+       regexp_match(original_text, '\r\n(?:\w+ \w+|\w+)\r\n(.*):') AS crime_type,
        regexp_match(original_text, ':\s(.+)(?:C0|SO)') AS description,
        regexp_match(original_text, '(?:C0|SO)[0-9]+') AS case_number
 FROM crime_reports;
@@ -139,59 +139,70 @@ FROM crime_reports;
 
 UPDATE crime_reports
 SET date_1 = 
-(
-    (regexp_match(original_text, '\d{1,2}\/\d{1,2}\/\d{2}'))[1]
+    to_timestamp(
+		(regexp_match(original_text, '\d{1,2}\/\d{1,2}\/\d{2}'))[1]
         || ' ' ||
-    (regexp_match(original_text, '\/\d{2}\n(\d{4})'))[1] 
-        ||' US/Eastern'
-)::timestamptz;
+		(regexp_match(original_text, '\/\d{2}\r\n(\d{4})'))[1],
+		'MM/DD/YY HH24MI'	
+	);
 
 SELECT crime_id,
-       date_1,
+       date_1 at time zone 'EST',
        original_text
-FROM crime_reports;
+	FROM crime_reports;
 
 -- Listing 13-10: Updating all crime_reports columns
 
+set timezone to 'US/Eastern';
 UPDATE crime_reports
-SET date_1 = 
-    (
-      (regexp_match(original_text, '\d{1,2}\/\d{1,2}\/\d{2}'))[1]
-          || ' ' ||
-      (regexp_match(original_text, '\/\d{2}\n(\d{4})'))[1] 
-          ||' US/Eastern'
-    )::timestamptz,
+SET date_1 = to_timestamp(
+	(regexp_match(original_text, '\d{1,2}\/\d{1,2}\/\d{2}'))[1]
+	|| ' ' ||
+	(regexp_match(original_text, '\/\d{2}\r\n(\d{4})'))[1],
+	'MM/DD/YY HH24MI'
+),
              
     date_2 = 
     CASE 
     -- if there is no second date but there is a second hour
-        WHEN (SELECT regexp_match(original_text, '-(\d{1,2}\/\d{1,2}\/\d{1,2})') IS NULL)
-                     AND (SELECT regexp_match(original_text, '\/\d{2}\n\d{4}-(\d{4})') IS NOT NULL)
+        WHEN 
+		(SELECT regexp_match(original_text, '-(\d{1,2}\/\d{1,2}\/\d{1,2})') IS NULL)
+		AND 
+		(SELECT regexp_match(original_text, '\/\d{2}\r\n\d{4}-(\d{4})') IS NOT NULL)
         THEN 
-          ((regexp_match(original_text, '\d{1,2}\/\d{1,2}\/\d{2}'))[1]
-              || ' ' ||
-          (regexp_match(original_text, '\/\d{2}\n\d{4}-(\d{4})'))[1] 
-              ||' US/Eastern'
-          )::timestamptz 
+		to_timestamp(
+			(regexp_match(original_text, '\d{1,2}\/\d{1,2}\/\d{2}'))[1]
+			|| ' ' ||
+			(regexp_match(original_text, '\/\d{2}\r\n\d{4}-(\d{4})'))[1],
+			'MM/DD/YY HH24MI'
+		) at time zone 'EST'
 
     -- if there is both a second date and second hour
-        WHEN (SELECT regexp_match(original_text, '-(\d{1,2}\/\d{1,2}\/\d{1,2})') IS NOT NULL)
-              AND (SELECT regexp_match(original_text, '\/\d{2}\n\d{4}-(\d{4})') IS NOT NULL)
+        WHEN 
+		(SELECT regexp_match(original_text, '-(\d{1,2}\/\d{1,2}\/\d{1,2})') IS NOT NULL)
+		AND 
+		(SELECT regexp_match(original_text, '\/\d{2}\r\n\d{4}-(\d{4})') IS NOT NULL)
         THEN 
-          ((regexp_match(original_text, '-(\d{1,2}\/\d{1,2}\/\d{1,2})'))[1]
-              || ' ' ||
-          (regexp_match(original_text, '\/\d{2}\n\d{4}-(\d{4})'))[1] 
-              ||' US/Eastern'
-          )::timestamptz 
+		to_timestamp(
+			(regexp_match(original_text, '-(\d{1,2}\/\d{1,2}\/\d{1,2})'))[1]
+			|| ' ' ||
+			(regexp_match(original_text, '\/\d{2}\r\n\d{4}-(\d{4})'))[1],
+			'MM/DD/YY HH24MI'
+		)
+		  
     -- if neither of those conditions exist, provide a NULL
         ELSE NULL 
     END,
-    street = (regexp_match(original_text, 'hrs.\n(\d+ .+(?:Sq.|Plz.|Dr.|Ter.|Rd.))'))[1],
-    city = (regexp_match(original_text,
-                           '(?:Sq.|Plz.|Dr.|Ter.|Rd.)\n(\w+ \w+|\w+)\n'))[1],
-    crime_type = (regexp_match(original_text, '\n(?:\w+ \w+|\w+)\n(.*):'))[1],
+	
+    street = (regexp_match(original_text, 'hrs.\r\n(\d+ .+(?:Sq.|Plz.|Dr.|Ter.|Rd.))'))[1],
+    city = (regexp_match(original_text,'(?:Sq.|Plz.|Dr.|Ter.|Rd.)\r\n(\w+ \w+|\w+)\r\n'))[1],
+    crime_type = (regexp_match(original_text, '\r\n(?:\w+ \w+|\w+)\r\n(.*):'))[1],
     description = (regexp_match(original_text, ':\s(.+)(?:C0|SO)'))[1],
     case_number = (regexp_match(original_text, '(?:C0|SO)[0-9]+'))[1];
+
+set timezone to 'Asia/Bangkok';
+select * from crime_reports;
+
 
 -- Listing 13-11: Viewing selected crime data
 
